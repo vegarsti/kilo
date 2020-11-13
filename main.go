0be73158a7e4fb1f86af36d834ee9cf3a6343132
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2020-11-13 08:10:50 +0100

    Step 2.3: read bytes until EOF

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
 func main() {
-	return
+	r := bufio.NewReader(os.Stdin)
+	for {
+		_, err := r.ReadByte()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "kilo: %v\n", err)
+		}
+	}
 }
