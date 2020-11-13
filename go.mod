ec0a5259fb78ec67b617253de18a6608f6dff7c0
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2020-11-13 08:33:34 +0100

    Step 2.5-2.7: Turn off echoing and canonical mode

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vegarsti/kilo
 
 go 1.15
+
+require golang.org/x/crypto v0.0.0-20201112155050-0c6587e931a9
--- main.go
+++ main.go
@@ -2,22 +2,49 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
-	"io"
 	"os"
+	"os/exec"
 )
 
+var originalSttyState bytes.Buffer
+
+func getSttyState(state *bytes.Buffer) error {
+	cmd := exec.Command("stty", "-g")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = state
+	return cmd.Run()
+}
+
+func setSttyState(state *bytes.Buffer) error {
+	cmd := exec.Command("stty", state.String())
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
+}
+
 func main() {
-	var c byte
 	var err error
+	err = getSttyState(&originalSttyState)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "kilo: %v\n", err)
+	}
+	defer setSttyState(&originalSttyState)
+
+	setSttyState(bytes.NewBufferString("cbreak")) // Turn off canonical mode
+	setSttyState(bytes.NewBufferString("-echo"))  // Turn off terminal echoing
+
 	r := bufio.NewReader(os.Stdin)
+	var c byte
 	for {
 		c, err = r.ReadByte()
-		if err == io.EOF || string(c) == "q" {
-			break
-		}
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "kilo: %v\n", err)
 		}
+		if c == 'q' {
+			break
+		}
+		fmt.Printf("%d", c)
 	}
 }
